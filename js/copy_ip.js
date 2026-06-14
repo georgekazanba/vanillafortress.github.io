cc874230b0cb57f1deb7b23bc6e90120d8c5a740
_Spark <spark-aur@proton.me>
2026-06-14 16:10:37 +0300

    update server cards
    javascript bullshit coming right up!

---
+++ js/copy_ip.js
@@ -0,0 +1,27 @@
+// god i hate javascript
+// anything for the website i suppose
+
+document.addEventListener("DOMContentLoaded", () => { // WHY THE FUCK IS IT SO NESTED
+  document.querySelectorAll(".copy-ip").forEach(btn => {
+    btn.addEventListener("click", async (e) => {
+      e.preventDefault();
+      
+      const text = btn.dataset.copy;
+      if (!text) return;
+
+      try {
+        await navigator.clipboard.writeText(text);
+
+        const old = btn.textContent;
+        btn.textContent = "Copied!";
+
+        setTimeout(() => {
+          btn.textContent = old;
+        }, 1000);
+      } catch (err) {
+        console.log("failure", err);
+      }
+
+    })
+  })
+}) // THIS FILE HAS TWELVE CURLY BRACES
